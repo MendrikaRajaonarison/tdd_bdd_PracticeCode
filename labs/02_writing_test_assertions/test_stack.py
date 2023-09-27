a98d55bf180af6db53b067780492e50fffe44662
MendrikaRajaonarison <115612748+MendrikaRajaonarison@users.noreply.github.com>
2023-09-27 16:31:15 +0300

    Update test_stack.py

--- labs/02_writing_test_assertions/test_stack.py
+++ labs/02_writing_test_assertions/test_stack.py
@@ -14,16 +14,28 @@ class TestStack(TestCase):
 
     def test_push(self):
         """Test pushing an item into the stack"""
-        raise Exception("not implemented")
+        self.stack.push("Hello")
+        self.stack.push("World")
+        self.assertEqual(self.stack.peek(), "World")
+        self.assertEqual(self.stack.pop(), "World")
 
     def test_pop(self):
         """Test popping an item of off the stack"""
-        raise Exception("not implemented")
+        self.stack.push("Hello")
+        self.stack.push("World")
+        self.assertEqual(self.stack.pop(), "World")
+        self.assertEqual(self.stack.peek(), "Hello")
+        self.assertEqual(self.stack.pop(), "Hello")
+        self.assertTrue(self.stack.is_empty())
 
     def test_peek(self):
         """Test peeking at the top the stack"""
-        raise Exception("not implemented")
+        self.stack.push("Magic")
+        self.stack.push("Mike")
+        self.assertEqual(self.stack.peek(), "Mike")
 
     def test_is_empty(self):
         """Test if the stack is empty"""
-        raise Exception("not implemented")
+        self.assertTrue(self.stack.is_empty())
+        self.stack.push("Hello")
+        self.assertFalse(self.stack.is_empty())
